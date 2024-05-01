d558a757ce46e7ea22de8becdcaca40113df2327
hsuktech <107915868+hsuktech@users.noreply.github.com>
2024-05-01 11:24:37 +0100

    Update RunLocalCode.ps1

--- RunLocalCode.ps1
+++ RunLocalCode.ps1
@@ -1,11 +1,15 @@
 iex (irm https://raw.githubusercontent.com/hsuktech/hsuktech.github.io/main/LocalCode.ps1)
 
-if($webhookUri -eq $null){
-  $webhookUri = Read-Host "Webhook Uri"
+if(Test-Path variable:global:webhookUri){
+    # Do nothing
+} else { 
+    $webhookUri = Read-Host "Webhook Uri"
 }
 
-if($sharedSecret -eq $null){
-  $sharedSecret = Read-Host "Shared Secret"
+if(Test-Path variable:global:sharedSecret){
+    # Do nothing
+} else { 
+    $sharedSecret = Read-Host "Shared Secret"
 }
 
 $scriptName = Read-Host "Script"
