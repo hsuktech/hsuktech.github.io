b3595860d461defb7001af911cab5551729ce592
hsuktech <107915868+hsuktech@users.noreply.github.com>
2024-05-01 11:15:33 +0100

    Update RunLocalCode.ps1

--- RunLocalCode.ps1
+++ RunLocalCode.ps1
@@ -1,6 +1,12 @@
 iex (irm https://raw.githubusercontent.com/hsuktech/hsuktech.github.io/main/LocalCode.ps1)
 
-$webhookUri = Read-Host "Webhook Uri"
-$sharedSecret = Read-Host "Shared Secret"
+if($webhookUri -eq ""){
+  $webhookUri = Read-Host "Webhook Uri"
+}
+
+if($sharedSecret -eq ""){
+  $sharedSecret = Read-Host "Shared Secret"
+}
+
 $scriptName = Read-Host "Script"
 RunLocalCode -scriptName $scriptName -sharedSecret $sharedSecret -webhookUri $webhookUri
