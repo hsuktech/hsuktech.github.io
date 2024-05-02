32b60ae2b2ed739e7cf163f8045f0851c3551757
hsuktech <107915868+hsuktech@users.noreply.github.com>
2024-05-02 14:52:56 +0100

    Update LocalCode.ps1

--- LocalCode.ps1
+++ LocalCode.ps1
@@ -14,6 +14,18 @@ param(
     #$sharedSecret = ""
     #$webhookUri = ""
 
+    if(Test-Path variable:global:webhookUri){
+    # Do nothing
+    } else { 
+        $webhookUri = Read-Host "Webhook Uri"
+    }
+    
+    if(Test-Path variable:global:sharedSecret){
+        # Do nothing
+    } else { 
+        $sharedSecret = Read-Host "Shared Secret"
+    }
+
     $body = @"
     {
         "`$sharedSecret": "$sharedSecret", 
