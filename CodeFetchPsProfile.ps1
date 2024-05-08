5ab62b770ede024e1b348374e34a1ebe6bc1fab4
hsuktech <107915868+hsuktech@users.noreply.github.com>
2024-05-08 09:17:53 +0100

    Update CodeFetchPsProfile.ps1

--- CodeFetchPsProfile.ps1
+++ CodeFetchPsProfile.ps1
@@ -4,10 +4,10 @@ cls
 iex (irm https://raw.githubusercontent.com/hsuktech/hsuktech.github.io/main/CodeFetch.ps1)
 
 # Set the variables
-$webhookUri = Read-Host "Webhook Uri"
+$codeFetchUri = Read-Host "Code Fetch Uri"
 $sharedSecret = Read-Host "Shared Secret"
 
 # Fetch the code
 
 # Create standard PowerShell profile for the current user 
-CodeFetch -scriptName ReACKs-CodeFetchPsProfile.ps1 -sharedSecret $sharedSecret -webhookUri $webhookUri
+CodeFetch -scriptName ReACKs-CodeFetchPsProfile.ps1 -sharedSecret $sharedSecret -webhookUri $codeFetchUri
