7c052ee99ff5b1a370ae140ed7f3c4f3faf5a03d
Simon Shaw <simon.shaw@handstand.co.uk>
2024-07-25 10:18:55 +0100

    Updates

--- CodeFetch.ps1
+++ CodeFetch.ps1
@@ -48,7 +48,7 @@ param(
     if($saveLocal){
         # Save
         $scriptText = $code.ScriptText 
-        $scriptText | Out-File "C:\Assets\test.ps1"
+        $scriptText | Out-File "C:\Assets\$scriptName.ps1"
     } else {
         # Run
         Invoke-Expression $code.ScriptText
