fe111453333339d3676b023659c3a4aac569d6d2
Simon Shaw <simon.shaw@handstand.co.uk>
2024-07-30 09:17:30 +0100

    Updates

--- TraccVMConfig.ps1
+++
@@ -1,5 +0,0 @@
-# Run VMEditor.exe
-CodeFetch -scriptName TRACC-VMEditor.ps1 -sharedSecret $sharedSecret -codeFetchUri $codeFetchUri
-
-# Resize VM
-CodeFetch -scriptName TRACC-ResizeVM.ps1 -sharedSecret $sharedSecret -codeFetchUri $codeFetchUri
