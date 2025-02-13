6744c2d0487094341d95751ddeacaeb2b1864eef
hsuktech <technical@handstand.co.uk>
2025-02-13 10:26:50 +0000

    Updates

--- CodeFetch-Helpers.ps1
+++ CodeFetch-Helpers.ps1
@@ -1,4 +1,6 @@
-# This function tidies up the cloned repository
+# TODO: Load this helper from CodeFetch!! #codefetch
+
+# This function tidies up the cloned repository to include only the files that are tagged
 function Clean-Repo {
 
     param (
@@ -61,3 +63,94 @@ function Clean-Repo {
     # Call function to clean empty folders
     Remove-EmptyFolders -Path "$searchPath"
 }   
+
+# This function indexes and encrypts the files to be copied
+function Process-Repo {
+    
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$sharedSecret,
+        [Parameter(Mandatory=$true)]
+        [string]$repoName
+
+    )
+
+    # Index path
+    $indexFile = "cf_index.json"
+    Remove-Item -Force $indexFile
+
+    # Metadata index
+    $indexJson = @()
+
+    # SHA1 hash of the repo name to add to the path
+    $repoNameHash = Get-SHA ($repoName) -algorithm "SHA1"
+
+    # Encrypt all remaining files using AES-256
+    Get-ChildItem -Path . -Recurse -File | ForEach-Object {
+        $inputFile = $_.FullName
+
+        # Read the first few lines of the file to extract metadata
+        $lines = Get-Content -Path $inputFile -TotalCount 10
+
+        # SHA1 hash of the file path for the CodeFetch ID
+        $filePathHash = Get-SHA ($inputFile) -algorithm "SHA1"   
+
+        # Get the relative file path 
+        $filePath = Resolve-Path -Path $inputFile -Relative
+        $filePath = ($filePath -replace '\\', '/') -replace '^\./', ''
+
+        # Build the path
+        $filePath = "$repoNameHash/$filePath"
+        
+        # Initialize a hashtable to store metadata for the current file
+        $metadata = @{
+            Name        = $null
+            Tags        = $null
+            Description = $null
+            CodeFetchId = $filePathHash
+            FilePath    = $filePath
+        }
+
+        # Parse lines for metadata
+        foreach ($line in $lines) {
+            if ($line -match "^#\s*CF-Name:\s*(.+)") {
+                $metadata.Name = $matches[1].Trim()
+            }
+            elseif ($line -match "^#\s*CF-Tags:\s*(.+)") {
+                $metadata.Tags = $matches[1].Trim() -split "\s*,\s*"
+            }
+            elseif ($line -match "^#\s*CF-Description:\s*(.+)") {
+                $metadata.Description = $matches[1].Trim()
+            }
+        }
+
+        # Add to the index
+        $indexJson += $metadata
+
+        # Encrypt the file
+        #$outputFile = "$inputFile.enc"
+        #openssl enc -aes-256-cbc -salt -pbkdf2 -in "$inputFile" -out "$outputFile" -pass pass:"$sharedSecret"
+        
+        # Remove the original file after encryption
+        #Remove-Item -Force "$inputFile"
+    }
+
+    # Convert the metadata array to JSON and write it to the output file
+    $indexJson | ConvertTo-Json -Depth 3 | Set-Content -Path $indexFile
+
+    $indexJson | ConvertTo-Json -Depth 3
+}
+
+# This function returns the SHA hash of a string
+function Get-SHA {
+    param (
+        [string]$inputString,
+        [string]$algorithm = "SHA256"
+    )
+
+    $hashAlgorithm = [System.Security.Cryptography.HashAlgorithm]::Create($algorithm)
+    $bytes = [System.Text.Encoding]::UTF8.GetBytes($inputString)
+    $hashBytes = $hashAlgorithm.ComputeHash($bytes)
+
+    return -join ($hashBytes | ForEach-Object { "{0:X2}" -f $_ })
+}
